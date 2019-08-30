da2e406dc1d3d111fdcea80306da52608968cd18
Necsuliu Emanuela <necsuliuemanuela@gmail.com>
2019-08-30 17:48:16 +0300

    JS Basics - Functions and Arrays

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -245,6 +245,7 @@ switch (true) {
 // falsy values: undefined, null, 0, '', NaN
 //truthy values: NOT falsy values
 
+/*
 var height;
 height = 23;
 
@@ -259,6 +260,138 @@ if (height || height === 0) {
 if(height === '23') {
 	console.log('The == operator does type coercion!');
 }
+*/
+
+/************************************
+* Challenge 2
+*/
+/*
+var averrageScoreJohn = (97 + 134 + 105) / 3;
+var averrageScoreMark = (116 + 94 + 123) / 3;
+var averrageScoreMary = (97 + 134 + 105) / 3;
+console.log(averrageScoreJohn, averrageScoreMark, averrageScoreMary);
+
+switch(true) {
+	case (averrageScoreJohn > averrageScoreMark) && (averrageScoreJohn > averrageScoreMary):
+		console.log('John Team wins in average score with ' + averrageScoreJohn + ' points.');
+		break;
+	case (averrageScoreMark > averrageScoreJohn) && (averrageScoreMark > averrageScoreMary):
+		console.log('Mark Team wins in average score with ' + averrageScoreMark + ' points.');
+		break;
+	case (averrageScoreMary > averrageScoreJohn) && (averrageScoreMary > averrageScoreMark):
+		console.log('Mary Team wins in average score with ' + averrageScoreMary + ' points.');
+		break;
+	case (averrageScoreJohn == averrageScoreMark) && (averrageScoreJohn > averrageScoreMary || averrageScoreMark > averrageScoreMary):
+		console.log('There is a draw between John\'s team and Mark\'s team with a score of ' + averrageScoreJohn + ' points.');
+		break;
+	case (averrageScoreJohn == averrageScoreMary) && (averrageScoreJohn > averrageScoreMark || averrageScoreMary > averrageScoreMark):
+		console.log('There is a draw between John\'s team and Mary\'s team with a score of ' + averrageScoreJohn + ' points.');
+		break;
+	case (averrageScoreMark == averrageScoreMary) && (averrageScoreMark > averrageScoreJohn || averrageScoreMary > averrageScoreJohn):
+		console.log('There is a draw between Mark\'s team and Mary\'s team with a score of ' + averrageScoreMark + ' points.');
+		break;
+	case averrageScoreJohn == averrageScoreMark == averrageScoreMary:
+		console.log('There is a draw with a score of' + averrageScoreMary + ' points.');
+		break;	
+	
+}
+*/
+/************************************
+* Functions
+*/
+/*
+function calculateAge(birthYear) {
+	return 2018 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+	var age = calculateAge(year);
+	var retirement = 65 - age;
+	
+	if (retirement > 0) {
+		console.log(firstName + ' retires in ' + retirement + ' years.');
+	} else {
+		console.log(firstName + ' is already retired');
+	}
+	
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
+*/
+
+/************************************
+* Functions Statements and Expressions
+*/
+/*
+//Function Expression
+var whatDoYouDo = function(job, firstName) {
+	switch(job) {
+		case 'teacher':
+			return firstName + ' teaches kids how to code';
+		case 'driver': 
+			return firstName + ' drives a uber in Lisbon';
+		case 'designer':
+			return firstName + ' designs beautiful websites';
+		default:
+			return firstName + ' does something else.';
+			
+	}
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+*/
+
+/************************************
+* Arrays
+*/
+
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+//Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+
+console.log(isDesigner);
+
+/************************************
+* Challenge 3
+*/
+
+
+
+
+
+
+
 
 
 
