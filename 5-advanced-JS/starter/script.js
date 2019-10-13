a4b97455e8b07c6f1a54547f9a134b6294e46981
Necsuliu Emanuela <necsuliuemanuela@gmail.com>
2019-10-14 00:00:01 +0300

    bind, call, applay

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -183,7 +183,7 @@ game();
 */
 
 // Closures
-
+/*
 function retirement(retirementAge) {
 	var a = ' years left until retirement';
 	return function(yearOfBirth) {
@@ -217,6 +217,71 @@ function interviewQuestions(job) {
 
 
 interviewQuestions('teacher')('John');
+*/
+
+// Lecture: Bind, call and apply
+
+
+
+var john ={
+	name: 'john',
+	age: 26,
+	job: 'teacher',
+	presentation: function(style, timeOfDay) {
+		if (style === 'formal') {
+			console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+		} else if(style === 'friendly') {
+			console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+		}
+	}
+};
+
+var emily = {
+	name: 'Emily',
+	age: 35,
+	job: 'designer'
+};
+
+
+john.presentation('formal', 'morning');
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+johnFriendly('morning');johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+// bind
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+	var arrRes = [];
+	for (var i = 0; i < arr.length; i++) {
+		arrRes.push(fn(arr[i]));
+	}
+	return arrRes;
+}
+
+function calculateAge(el) {
+	return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+	return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
+
+
+
 
 
 
